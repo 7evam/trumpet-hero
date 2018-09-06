e18a53ce9966a698cfb904c34f7aa23ad898506e
Evan Lane <evanlane@ool-ad030017.dyn.optonline.net>
2018-09-06 14:20:40 -0400

    changed keys for valves, added currentValves function

--- script.js
+++ script.js
@@ -26,39 +26,109 @@ let body = document.body;
 
 //BIG shout out to Jeremy Kahn at https://github.com/jeremyckahn :) :) :)
 
+let currentValves = [];
+
 kd.run(function () {
   kd.tick();
 });
 
+//arrow keys don't work because of keyboard ghosting...
+
 kd.LEFT.down(function(){
   valve1.style.border="6px solid yellow";
-  console.log("left key down");
 });
 
 kd.LEFT.up(function(){
   valve1.style.border="6px solid white";
-    console.log("down key up")
 });
 
 kd.DOWN.down(function(){
   valve2.style.border="6px solid yellow";
-    console.log("down key down");
 });
 
 kd.DOWN.up(function(){
   valve2.style.border="6px solid white";
-    console.log("down key up");
 });
 
 kd.RIGHT.down(function(){
   valve3.style.border="6px solid yellow";
-    console.log("right key down");
 });
 
 kd.RIGHT.up(function(){
   valve3.style.border="6px solid white";
-    console.log("up key down");
 });
+
+
+function remove(array, element) {
+    const index = array.indexOf(element);
+    array.splice(index, 1);
+}
+
+//spacebar for breath
+
+const meter = document.querySelector(".meter")
+
+kd.run(function () {
+  kd.tick();
+});
+
+kd.SPACE.down(function(){
+  meter.style.width="0%";
+  meter.style.transition="17s";
+    valve1.style.border="6px solid black";
+    if(!currentValves.includes("air")){
+      currentValves.push("air");
+    }
+});
+
+kd.SPACE.up(function(){
+  meter.style.width="100%";
+  meter.style.transition="2s";
+  remove(currentValves,"air");
+});
+
+//need to use these keys because of keyboard ghosting
+
+kd.PERIOD.down(function(){
+    valve1.style.border="6px solid black";
+    if(!currentValves.includes(1)){
+      currentValves.push(1);
+    }
+});
+
+kd.PERIOD.up(function(){
+    valve1.style.border="6px solid white";
+    remove(currentValves,1)
+});
+
+
+kd.QMARK.down(function(){
+    valve2.style.border="6px solid black";
+    if(!currentValves.includes(2)){
+    currentValves.push(2);
+  }
+});
+
+kd.QMARK.up(function(){
+    valve2.style.border="6px solid white";
+    remove(currentValves,2)
+});
+
+kd.SHIFT.down(function(){
+  valve3.style.border="6px solid black";
+  if(!currentValves.includes(3)){
+  currentValves.push(3);
+  }
+});
+
+kd.SHIFT.up(function(){
+  valve3.style.border="6px solid white";
+  remove(currentValves,3)
+});
+
+setInterval(function(){ 
+  console.log(currentValves) 
+}, 1000);
 
 
 //collision logic
